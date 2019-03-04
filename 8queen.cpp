a1bb141fd1a3145a725282329302b75cc447b232
chenzifeng1 <chenzifeng527@gmail.com>
2019-03-04 22:48:19 +0800

    打卡：八皇后问题，还有点小问题

--- 8queen.cpp
+++ 8queen.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <iostream>
 #include <math.h>
+#include <stdlib.h>
 
 using namespace std;
 const int maxi=11;
@@ -11,12 +12,11 @@ int n,P[maxi];
 void generateQ(int index);
 
 int main(){
-
-
     cin>>n;
-    if(n>=1)
-        int row[n],col[n];//行列
-
+    generateQ(1);
+    for(int i=0;i<n;i++){
+        cout<<P[i];
+    }
 }
 
 void generateQ(int index){
@@ -24,11 +24,20 @@ void generateQ(int index){
         counter++;
         return ;
     }
-    for(int x=0;x<n;x++){
+    for(int x=1;x<=n;x++){
         if(hashTable[x]==false){
             bool flag=true;
             for(int pre=1;pre<index;pre++){
-
+                if(abs(index-pre)==abs(x-P[pre])){  //两个皇后的行列之差的绝对值相等则说明两者在同一个对角线上
+                    flag=false;
+                    break;
+                }
+            }
+            if(flag){
+                P[index]=x;//将index位皇后放在x行
+                hashTable[x]=true;//将x行的标志置为true，表示已经有皇后占领
+                generateQ(index+1);//递归测试index+1位
+                hashTable[x]=false;//递归测试结束，将原标记置回
             }
         }
     }
